29065c41326d2e654a71a5a6bfba13eb33b8dc44
profesor <profesor@DESKTOP-QDMJOE6>
2017-11-17 10:52:59 +0100

    Cambio a Adios

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Debug.WriteLine("Hola")
+        Debug.WriteLine("Adios")
     End Sub
 End Class
